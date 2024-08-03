829cd774dee4e1e5e3423bed950a57ba2e14f4f9
Гунарді Сутанто <lonecalvary78.de@gmail.com>
2024-08-04 00:22:11 +0800

    Add mocked tests for Seller module

---
+++ src/test/java/demo/app/car/controller/MockedSellerControllerTest.java
@@ -0,0 +1,51 @@
+package demo.app.car.controller;
+
+import demo.app.car.domain.seller.controller.SellerController;
+import demo.app.car.domain.seller.exception.DuplicateSellerException;
+import demo.app.car.domain.seller.exception.NonExistingSellerException;
+import demo.app.car.domain.seller.model.SellerDTO;
+import demo.app.car.domain.seller.service.SellerService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MockedSellerControllerTest {
+  @InjectMocks
+  SellerController sellerController;
+
+  @Mock
+  SellerService sellerService;
+
+  @Test
+  @DisplayName("Find seller profile By Unique ID")
+  void findProfileById() throws NonExistingSellerException {
+    var sellerId = 1L;
+    when(sellerService.findProfileById(any())).thenReturn(newDummySellerDTO());
+    Assertions.assertAll(
+            ()->Assertions.assertDoesNotThrow(()->sellerController.findProfileById(sellerId)),
+            ()->Assertions.assertNotNull(sellerController.findProfileById(sellerId))
+    );
+    verify(sellerService, times(2)).findProfileById(any());
+  }
+
+  @Test
+  @DisplayName("Create new Seller profile")
+  void createNewSellerProfile() throws DuplicateSellerException {
+    var sellerDTO = new SellerDTO(null,"John","Doe", "john.doe@testmail.com");
+    when(sellerService.createNewProfile(any())).thenReturn(newDummySellerDTO());
+    Assertions.assertDoesNotThrow(()->sellerController.newProfile(sellerDTO));
+    verify(sellerService).createNewProfile(any());
+  }
+
+  private SellerDTO newDummySellerDTO() {
+    return new SellerDTO(1L,"John","Doe", "john.doe@testmail.com");
+  }
+}
---
+++ src/test/java/demo/app/car/domain/seller/service/MockedSellerServiceTest.java
@@ -0,0 +1,55 @@
+package demo.app.car.domain.seller.service;
+
+import demo.app.car.domain.seller.entity.Seller;
+import demo.app.car.domain.seller.exception.DuplicateSellerException;
+import demo.app.car.domain.seller.exception.NonExistingSellerException;
+import demo.app.car.domain.seller.model.SellerDTO;
+import demo.app.car.domain.seller.repository.SellerRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MockedSellerServiceTest {
+  @InjectMocks
+  SellerService sellerService;
+
+  @Mock
+  SellerRepository sellerRepository;
+
+  @Test
+  @DisplayName("Find seller profile By Unique ID")
+  void findProfileById() throws NonExistingSellerException {
+    var sellerId = 1L;
+    when(sellerRepository.findById(any())).thenReturn(newDummySeller());
+    Assertions.assertAll(
+            ()->Assertions.assertDoesNotThrow(()->sellerService.findProfileById(sellerId)),
+            ()->Assertions.assertNotNull(sellerService.findProfileById(sellerId))
+    );
+    verify(sellerRepository, times(2)).findById(any());
+  }
+
+  @Test
+  @DisplayName("Create new Seller profile")
+  void createNewSellerProfile() throws DuplicateSellerException {
+    var sellerDTO = new SellerDTO(null,"John","Doe", "john.doe@testmail.com");
+    when(sellerRepository.createNewProfile(any())).thenReturn(newDummySeller());
+    Assertions.assertDoesNotThrow(()->sellerService.createNewProfile(sellerDTO));
+    verify(sellerRepository).createNewProfile(any());
+  }
+  private Seller newDummySeller() {
+    var seller = new Seller();
+    seller.setId(1L);
+    seller.setFirstName("John");
+    seller.setLastName("Doe");
+    seller.setEmail("john.doe@testmail.com");
+    return seller;
+  }
+}

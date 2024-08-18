98f0b5238b1b37847f2db38c0558699185859e5d
Гунарді Сутанто <lonecalvary78.de@gmail.com>
2024-08-18 13:19:12 +0800

    Add null checking on Seller DTO

--- src/main/java/demo/app/car/domain/seller/model/SellerDTO.java
+++ src/main/java/demo/app/car/domain/seller/model/SellerDTO.java
@@ -1,6 +1,18 @@
 package demo.app.car.domain.seller.model;
 
+import jakarta.json.bind.annotation.JsonbPropertyOrder;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record SellerDTO(Long id, @NotBlank String firstName, @NotBlank String lastName, @NotBlank String email) {
+@JsonbPropertyOrder({"id","firstName","lastName","email","createdAt","lastModifiedAt"})
+public record SellerDTO(Long id,
+                        @NotNull
+                        @NotBlank
+                        String firstName,
+                        @NotNull
+                        @NotBlank
+                        String lastName,
+                        @NotNull
+                        @NotBlank
+                        String email) {
 }
